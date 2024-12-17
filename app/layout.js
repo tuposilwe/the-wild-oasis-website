7b47a2ff45925149091d314499a08741ecec78bb
tuposilwe <rudigermkondya@gmail.com>
2024-12-17 15:55:58 +0300

    Building the Home Page

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${josefin.className}
-      bg-primary-950 text-primary-100 min-h-screen flex flex-col
+      bg-primary-950 text-primary-100 min-h-screen flex flex-col relative
       `}
       >
         <Header />
