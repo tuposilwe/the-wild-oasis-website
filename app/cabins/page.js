f59ba32dcee590c86439baf73f2d049997170222
tuposilwe <rudigermkondya@gmail.com>
2024-12-25 10:52:58 +0300

    streaming UI with supsense cabin list

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,16 +1,13 @@
 import Navigation from "@/app/_components/Navigation";
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabin, getCabins } from "../_lib/data-service";
+import CabinList from "../_components/CabinList";
+import { Suspense } from "react";
+import Spinner from "@/app/_components/Spinner";
 
 export const metadata = {
   title: "Cabins ",
 };
 
-export default async function Page() {
-  // CHANGE
-  const cabins =  await getCabins();
-
-  
+export default function Page() {
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -20,18 +17,14 @@ export default async function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
