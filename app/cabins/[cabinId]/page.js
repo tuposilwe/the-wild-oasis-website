12ee9447b4467b571fb7b15637c1bd568a4d4ab6
tuposilwe <rudigermkondya@gmail.com>
2024-12-28 13:09:27 +0300

    updated next js to 15 and added static site generation

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,14 +1,26 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
 export async function generateMetadata({ params }) {
-  const { name } = await getCabin(params.cabinId);
+  const { cabinId } = await params;
+
+  const { name } = await getCabin(cabinId);
   return { title: `Cabin ${name}` };
 }
 
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return ids;
+}
+
 export default async function Page({ params }) {
-  const cabin = await getCabin(params.cabinId);
+  const { cabinId } = await params;
+
+  const cabin = await getCabin(cabinId);
 
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
