102de3098c382d623b4118d187883903a9b522c7
tuposilwe <rudigermkondya@gmail.com>
2025-01-04 12:04:59 +0300

    creating a new guest on first sign in

--- app/account/page.js
+++ app/account/page.js
@@ -5,13 +5,14 @@ export const metadata = {
 };
 
 async function Page() {
-const session = await auth();
-const firstName = session.user.name.split(' ').at(0)
+  const session = await auth();
+
+  const firstName = session.user.name.split(" ").at(0);
 
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-       Welcome , {firstName}
-  </h2>
+      Welcome , {firstName}
+    </h2>
   );
 }
 
