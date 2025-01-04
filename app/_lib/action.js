ddcb5f2cc6b72624b5ab9859046d05ec6e523a8d
tuposilwe <rudigermkondya@gmail.com>
2025-01-04 11:34:07 +0300

    Building a custom sign out button

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -1,7 +1,11 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 export async function SignInAction() {
   await signIn("google", { redirectTo: "/account" });
+}
+
+export async function SignOutAction() {
+  await signOut({ redirectTo: "/" });
 }
