3dad65dccc578a38835a81d4e365f5e744f6ce83
tuposilwe <rudigermkondya@gmail.com>
2025-01-06 17:28:29 +0300

    Creating a new Reservation

--- app/_components/ReservationList.js
+++ app/_components/ReservationList.js
@@ -1,20 +1,20 @@
 "use client";
 
 import { useOptimistic } from "react";
-import { deleteReservation } from "../_lib/actions";
+import { deleteBooking } from "../_lib/actions";
 import ReservationCard from "./ReservationCard";
 
 function ReservationList({ bookings }) {
   const [optimisticBookings, optimisticDelete] = useOptimistic(
     bookings,
-    (curBookings,bookingId) => {
-      return curBookings.filter((booking) => booking.id !== bookingId)
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
     }
   );
 
   async function handleDelete(bookingId) {
     optimisticDelete(bookingId);
-    await deleteReservation(bookingId);
+    await deleteBooking(bookingId);
   }
 
   return (
